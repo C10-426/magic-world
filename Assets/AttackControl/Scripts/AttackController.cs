03b07013261be28429a23b6109d7fbdf0dfbb35c
acbron <136110473@qq.com>
2018-01-06 18:17:45 +0800

    [gentleh] add animator and attack action

---
+++ Assets/AttackControl/Scripts/AttackController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AttackController : MonoBehaviour, IPointerClickHandler {
+
+	public event Action OnAttackClicked;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
+		if (OnAttackClicked != null) {
+			OnAttackClicked ();
+		}
+	}
+}
